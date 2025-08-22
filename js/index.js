d0846f99962b1c87375ba24732807362ddae16e2
malarvizhi <sdevadharshini7@gmail.com>
2025-08-22 17:06:05 +0530

    pto

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-    function goToIndex() {
-      console.log("Button clicked ✅"); // Debug log
-      window.location.href = "../MindEase/html/password.html";
-    }
+    // function goToIndex() {
+    //   console.log("Button clicked ✅"); // Debug log
+    //   window.location.href = "../MindEase/html/password.html";
+    // }
